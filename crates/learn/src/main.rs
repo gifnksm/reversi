94316a305f2392c62ed197738aa4a7c0e1cd408b
gifnksm <makoto.nksm+github@gmail.com>
2021-09-23 18:03:07 +0900

    learn: refactoring

--- crates/learn/src/main.rs
+++ crates/learn/src/main.rs
@@ -26,57 +26,59 @@ struct Summary {
     interval_thinking_time: Duration,
     interval_visited_nodes: u64,
     interval_dist_sum: i32,
-    interval_dist_count: i32,
+    interval_game_count: i32,
     total_thinking_time: Duration,
     total_visited_nodes: u64,
+    current_iteration: u32,
+    total_iteration: u32,
 }
 
 impl Summary {
-    fn new() -> Self {
+    fn new(total_iteration: u32) -> Self {
         Self {
             interval_thinking_time: Duration::ZERO,
             interval_visited_nodes: 0,
             interval_dist_sum: 0,
-            interval_dist_count: 0,
+            interval_game_count: 0,
             start: Instant::now(),
             total_thinking_time: Duration::ZERO,
             total_visited_nodes: 0,
+            current_iteration: 0,
+            total_iteration,
         }
     }
 
-    fn add_play_result(&mut self, elapsed: Duration, visited_nodes: u32) {
+    fn add_result(&mut self, elapsed: Duration, visited_nodes: u32, dist: i32) {
+        self.current_iteration += 1;
         self.total_thinking_time += elapsed;
         self.total_visited_nodes += u64::from(visited_nodes);
         self.interval_thinking_time += elapsed;
         self.interval_visited_nodes += u64::from(visited_nodes);
-    }
-
-    fn add_update_result(&mut self, dist: i32) {
         self.interval_dist_sum += dist;
-        self.interval_dist_count += 1;
+        self.interval_game_count += 1;
     }
 
-    fn print_iteration(&mut self, current_iteration: u32, total_iteration: u32) {
+    fn print_iteration(&mut self) {
         let elapsed = self.start.elapsed();
-        let progress = f64::from(current_iteration) / f64::from(total_iteration);
+        let progress = f64::from(self.current_iteration) / f64::from(self.total_iteration);
 
         eprintln!(
             "{:8} / {:8} ({:5.1}%) (Estimated: {} / {}) ({} nodes, {:.3} sec, {:.2} kNPs) (AVG dist {})",
-            current_iteration,
-            total_iteration,
+            self.current_iteration,
+            self.total_iteration,
             progress * 100.0,
             DurationDisplay(elapsed),
             DurationDisplay(elapsed.div_f64(progress)),
             self.interval_visited_nodes,
             self.interval_thinking_time.as_secs_f64(),
             self.interval_visited_nodes as f64 / self.interval_thinking_time.as_secs_f64() / 1000.0,
-            (self.interval_dist_sum + self.interval_dist_count / 2) / self.interval_dist_count
+            (self.interval_dist_sum + self.interval_game_count / 2) / self.interval_game_count
         );
 
         self.interval_thinking_time = Duration::ZERO;
         self.interval_visited_nodes = 0;
         self.interval_dist_sum = 0;
-        self.interval_dist_count = 0;
+        self.interval_game_count = 0;
     }
 
     fn print_total(self) {
@@ -109,35 +111,35 @@ impl fmt::Display for DurationDisplay {
 
 type Error = Box<dyn std::error::Error>;
 
-fn main() -> Result<(), Error> {
-    let mut rng = rand::thread_rng();
+fn div_ceil(n: u32, m: u32) -> u32 {
+    (n + m - 1) / m
+}
 
+fn main() -> Result<(), Error> {
     let args: Args = argh::from_env();
     let evaluator = read_evaluator(&args)?;
     let com = Com::new(4, 12, 12);
     let mut updater = WeightUpdater::new(evaluator);
-    let mut summary = Summary::new();
 
-    let mut history = Vec::with_capacity(64);
-    for i in 0..args.num_iteration {
-        let board = play_game(
-            &mut rng,
-            updater.evaluator(),
-            &com,
-            &mut history,
-            &mut summary,
-        );
-        let avg_dist = update(&mut updater, &board, &mut history);
-        summary.add_update_result(avg_dist);
+    const FLUSH_INTERVAL: u32 = 10;
+    const ITERATION_INTERVAL: u32 = 10 * FLUSH_INTERVAL;
 
-        if (i + 1) % 10 == 0 {
+    let total_iteration = div_ceil(args.num_iteration, ITERATION_INTERVAL) * ITERATION_INTERVAL;
+    let mut summary = Summary::new(total_iteration);
+
+    for _ in 0..total_iteration / ITERATION_INTERVAL {
+        for _ in 0..ITERATION_INTERVAL / FLUSH_INTERVAL {
+            let evaluator = updater.evaluator().clone();
+            (0..FLUSH_INTERVAL)
+                .map(|_| play_game(&evaluator, &com))
+                .for_each(|(board, history, elapsed, visited_nodes)| {
+                    let avg_dist = update(&mut updater, &board, &history);
+                    summary.add_result(elapsed, visited_nodes, avg_dist);
+                });
             updater.flush();
         }
-
-        if (i + 1) % 100 == 0 {
-            write_evaluator(&args, updater.evaluator())?;
-            summary.print_iteration(i + 1, args.num_iteration);
-        }
+        write_evaluator(&args, updater.evaluator())?;
+        summary.print_iteration();
     }
     write_evaluator(&args, updater.evaluator())?;
     summary.print_total();
@@ -181,19 +183,19 @@ fn write_evaluator(args: &Args, evaluator: &WeightEvaluator) -> Result<(), Error
 }
 
 fn play_game(
-    rng: &mut impl Rng,
     evaluator: &WeightEvaluator,
     com: &Com,
-    history: &mut Vec<(Board, Color)>,
-    summary: &mut Summary,
-) -> Board {
+) -> (Board, Vec<(Board, Color)>, Duration, u32) {
+    let mut rng = rand::thread_rng();
     let mut board = Board::new();
     let mut color = Color::Black;
+    let mut total_duration = Duration::ZERO;
+    let mut total_visited_nodes = 0;
 
-    history.clear();
+    let mut history = Vec::with_capacity(64);
 
     for _ in 0..8 {
-        if let Some(pos) = board.flip_candidates(color).choose(rng) {
+        if let Some(pos) = board.flip_candidates(color).choose(&mut rng) {
             board = board.flipped(color, pos).1;
             history.push((board, color));
         }
@@ -202,12 +204,13 @@ fn play_game(
 
     loop {
         let pos = if board.count(None) > 12 && rng.gen_ratio(1, 100) {
-            board.flip_candidates(color).choose(rng)
+            board.flip_candidates(color).choose(&mut rng)
         } else {
             let start = Instant::now();
             let next_move = com.next_move(evaluator, &board, color);
             let elapsed = start.elapsed();
-            summary.add_play_result(elapsed, next_move.visited_nodes);
+            total_duration += elapsed;
+            total_visited_nodes += next_move.visited_nodes;
             next_move.best_pos
         };
         match pos {
@@ -224,23 +227,24 @@ fn play_game(
             }
         }
     }
-    board
+    (board, history, total_duration, total_visited_nodes)
 }
 
-fn update(updater: &mut WeightUpdater, board: &Board, history: &mut Vec<(Board, Color)>) -> i32 {
+fn update(updater: &mut WeightUpdater, board: &Board, history: &[(Board, Color)]) -> i32 {
     let result = updater.evaluator().evaluate(board, Color::Black, true);
 
+    let mut history = history.iter().rev();
     let mut board = *board;
     while board.count(None) < 8 {
-        board = history.pop().unwrap().0;
+        board = history.next().unwrap().0;
     }
 
     let mut total_dist = 0;
     let mut count = 0;
     for _ in (board.count(None) as i8)..(Board::SIZE * Board::SIZE - 12) {
-        let (board, color) = history.pop().unwrap();
-        let diff = if color == Color::Black {
-            updater.update(&board, result)
+        let (board, color) = history.next().unwrap();
+        let diff = if *color == Color::Black {
+            updater.update(board, result)
         } else {
             updater.update(&board.reverse(), -result)
         };
